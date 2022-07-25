9418252985ab527852c923e225d90c21425e90c5
Francisco <francisco.riverosc@sansano.usm.cl>
2022-07-25 00:39:45 -0400

    L2

--- src/main.cpp
+++ src/main.cpp
@@ -134,8 +134,8 @@ int main(int argc, char *argv[]) {
   //char *nasap1 = "ARRLFM_S18_INT";
 
   //char *nasap1 = "ARRLFM_SD_AP";
-  //char *nasap1 = "ARRLFM_PEF_AP";
-  char *nasap1 = "ARRLFM_S18_AP";
+  char *nasap1 = "ARRLFM_PEF_AP";
+  //char *nasap1 = "ARRLFM_S18_AP";
 
   // char *nasap1 = "ARRLFM_SD_RLMN";
  //char *nasap1 = "ARRLFM_PEF_RLMN";
