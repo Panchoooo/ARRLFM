31f27fcad787169f1641e1af0fced84a1cff0d16
Francisco <francisco.riverosc@sansano.usm.cl>
2022-07-24 23:14:02 -0400

    L2

--- src/main.cpp
+++ src/main.cpp
@@ -138,8 +138,8 @@ int main(int argc, char *argv[]) {
   //char *nasap1 = "ARRLFM_S18_AP";
 
  //  char *nasap1 = "ARRLFM_SD_RLMN";
- char *nasap1 = "ARRLFM_PEF_RLMN";
-  //char *nasap1 = "ARRLFM_S18_RLMN";
+ //char *nasap1 = "ARRLFM_PEF_RLMN";
+  char *nasap1 = "ARRLFM_S18_RLMN";
 
   //char *nasap1 = "ARRLFM_SD_RUNS_AP";
   //char *nasap1 = "ARRLFM_PEF_RUNS_AP";
@@ -177,8 +177,8 @@ int main(int argc, char *argv[]) {
 
 
  //  Arrlfm< Asap< SDBV, uint64_t ,wt_rlmn<sd_vector<>,sd_vector<>::rank_1_type,sd_vector<>::select_1_type,wt_int<>   >  > > TWA(path_bin,bwt,civ,0);  
- Arrlfm< Asap<pef_vector_opt< rank_support_v5<1>, select_support_mcl<1>, 1024 >, uint64_t ,wt_rlmn<sd_vector<>,sd_vector<>::rank_1_type,sd_vector<>::select_1_type,wt_int<>   >  > > TWA(path_bin,bwt,civ,0);  
-  //Arrlfm< Asap< BVTtipo2<sdsl::s18::vector<>, sdsl::s18::rank_support<> , sdsl::s18::select_support<>>, uint64_t ,wt_rlmn<sd_vector<>,sd_vector<>::rank_1_type,sd_vector<>::select_1_type,wt_int<>   >  > > TWA(path_bin,bwt,civ,0);  
+ //Arrlfm< Asap<pef_vector_opt< rank_support_v5<1>, select_support_mcl<1>, 1024 >, uint64_t ,wt_rlmn<sd_vector<>,sd_vector<>::rank_1_type,sd_vector<>::select_1_type,wt_int<>   >  > > TWA(path_bin,bwt,civ,0);  
+  Arrlfm< Asap< BVTtipo2<sdsl::s18::vector<>, sdsl::s18::rank_support<> , sdsl::s18::select_support<>>, uint64_t ,wt_rlmn<sd_vector<>,sd_vector<>::rank_1_type,sd_vector<>::select_1_type,wt_int<>   >  > > TWA(path_bin,bwt,civ,0);  
 
 
   //Arrlfm< Asap< SDBV, uint64_t ,rl_runs< 64,wt_ap<> > > >  TWA(path_bin,bwt,civ,0);  
