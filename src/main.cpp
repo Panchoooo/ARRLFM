7496f9f5b0dccde1ba5c597c2c2e45eb9a83d734
Francisco <francisco.riverosc@sansano.usm.cl>
2022-07-26 19:36:51 -0400

    Patrones_Lectura

--- src/main.cpp
+++ src/main.cpp
@@ -129,9 +129,9 @@ int main(int argc, char *argv[]) {
   strcat(path_output,out_path); 
 
 
-   char *nasap1 = "ARRLFM_SD_INT";
+ //  char *nasap1 = "ARRLFM_SD_INT";
  //char *nasap1 = "ARRLFM_PEF_INT";
- //char *nasap1 = "ARRLFM_S18_INT";
+ char *nasap1 = "ARRLFM_S18_INT";
 
  // char *nasap1 = "ARRLFM_SD_AP";
  // char *nasap1 = "ARRLFM_PEF_AP";
@@ -169,9 +169,9 @@ int main(int argc, char *argv[]) {
 
 
 
-   Arrlfm< Asap< SDBV, uint64_t ,wt_int<>> >  TWA(path_bin,bwt,civ,0);  
+  // Arrlfm< Asap< SDBV, uint64_t ,wt_int<>> >  TWA(path_bin,bwt,civ,0);  
 //Arrlfm< Asap< pef_vector_opt< rank_support_v5<1>, select_support_mcl<1>, 1024 >, uint64_t ,wt_int<>> >  TWA(path_bin,bwt,civ,0);  
- //Arrlfm< Asap< BVTtipo2<sdsl::s18::vector<>, sdsl::s18::rank_support<> , sdsl::s18::select_support<>> , uint64_t ,wt_int<>> >  TWA(path_bin,bwt,civ,0);  
+ Arrlfm< Asap< BVTtipo2<sdsl::s18::vector<>, sdsl::s18::rank_support<> , sdsl::s18::select_support<>> , uint64_t ,wt_int<>> >  TWA(path_bin,bwt,civ,0);  
 
 
  //Arrlfm< Asap< SDBV, uint64_t ,wt_ap<>> >  TWA(path_bin,bwt,civ,0);  
