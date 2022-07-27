14a3deaaaaed8afd9e7df09862fa11b02dd90ca4
Francisco <francisco.riverosc@sansano.usm.cl>
2022-07-27 01:10:28 -0400

    wt

--- src/Baseline.cpp
+++ src/Baseline.cpp
@@ -118,7 +118,7 @@ Baseline<Estructura>::~Baseline ( void ) {
 
 
 template<class Estructura >
-Arrlfm<Estructura>::Baseline (char* file, int_vector<> bwt,int_vector<> c, unsigned method ) {
+Baseline<Estructura>::Baseline (char* file, int_vector<> bwt,int_vector<> c, unsigned method ) {
     C = c;
     BWT = bwt;
 }
