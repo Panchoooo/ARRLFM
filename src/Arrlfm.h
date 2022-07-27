e0c9f7b22c17a7d40b90bdc3fd296aecb74b2e43
Francisco <francisco.riverosc@sansano.usm.cl>
2022-07-27 00:46:23 -0400

    wt

--- src/Arrlfm.h
+++ src/Arrlfm.h
@@ -50,7 +50,7 @@ public:
   int_vector<> BWT; // barrows wheeler
 
   Arrlfm<Estructura> ( char*,int_vector<>,int_vector<>,uint64_t);
-  Arrlfm<Estructura> ( int_vector<>,unsigned);:
+  Arrlfm<Estructura> ( int_vector<>,unsigned);
   Arrlfm<Estructura> ( string, unsigned);
   ~Arrlfm<Estructura> (void);
 
