5aa5a15353f5c7cc0a17f61205f43d31369e7b37
Francisco <francisco.riverosc@sansano.usm.cl>
2022-07-27 21:02:02 -0400

    pef

--- src/main.cpp
+++ src/main.cpp
@@ -152,7 +152,7 @@ int main(int argc, char *argv[]) {
 
   // wt RLMN AP
   // char *nasap1 = "ARRLFM_SD_RLMN_AP";
-   // char *nasap1 = "ARRLFM_PEF_RLMN_AP";
+    char *nasap1 = "ARRLFM_PEF_RLMN_AP";
   //char *nasap1 = "ARRLFM_S18_RLMN_AP";
 
   // rl RUNS ap
@@ -162,7 +162,7 @@ int main(int argc, char *argv[]) {
 
   // rl RUNS rlmn
    // char *nasap1 = "ARRLFM_SD_RUNS_RLMN";
-   char *nasap1 = "ARRLFM_PEF_RUNS_RLMN";
+   //char *nasap1 = "ARRLFM_PEF_RUNS_RLMN";
   //char *nasap1 = "ARRLFM_S18_RUNS_RLMN";
 
 
@@ -197,7 +197,7 @@ int main(int argc, char *argv[]) {
   
   // wt RLMN AP
    //Arrlfm< Asap< SDBV, uint64_t ,wt_rlmn<sd_vector<>,sd_vector<>::rank_1_type,sd_vector<>::select_1_type,wt_ap<>   >  > > TWA(path_bin,bwt,civ,0);  
-  // Arrlfm< Asap<pef_vector_opt< rank_support_v5<1>, select_support_mcl<1>, 1024 >, uint64_t ,wt_rlmn<sd_vector<>,sd_vector<>::rank_1_type,sd_vector<>::select_1_type,wt_ap<>   >  > > TWA(path_bin,bwt,civ,0);  
+   Arrlfm< Asap<pef_vector_opt< rank_support_v5<1>, select_support_mcl<1>, 1024 >, uint64_t ,wt_rlmn<sd_vector<>,sd_vector<>::rank_1_type,sd_vector<>::select_1_type,wt_ap<>   >  > > TWA(path_bin,bwt,civ,0);  
  //Arrlfm< Asap< BVTtipo2<sdsl::s18::vector<>, sdsl::s18::rank_support<> , sdsl::s18::select_support<>>, uint64_t ,wt_rlmn<sd_vector<>,sd_vector<>::rank_1_type,sd_vector<>::select_1_type,wt_ap<>   >  > > TWA(path_bin,bwt,civ,0);  
   
   // rl RUNS AP
@@ -208,7 +208,7 @@ int main(int argc, char *argv[]) {
 
   // rl RUNS rlmn ap
   //Arrlfm< Asap< SDBV, uint64_t ,rl_runs< 64,wt_rlmn<sd_vector<>,sd_vector<>::rank_1_type,sd_vector<>::select_1_type,wt_int<>  > > > >  TWA(path_bin,bwt,civ,0);  
-  Arrlfm< Asap< pef_vector_opt< rank_support_v5<1>, select_support_mcl<1>, 1024 >, uint64_t ,rl_runs< 64,wt_rlmn<sd_vector<>,sd_vector<>::rank_1_type,sd_vector<>::select_1_type,wt_int<>  > > > >  TWA(path_bin,bwt,civ,0);  
+ // Arrlfm< Asap< pef_vector_opt< rank_support_v5<1>, select_support_mcl<1>, 1024 >, uint64_t ,rl_runs< 64,wt_rlmn<sd_vector<>,sd_vector<>::rank_1_type,sd_vector<>::select_1_type,wt_int<>  > > > >  TWA(path_bin,bwt,civ,0);  
    //Arrlfm< Asap< BVTtipo2<sdsl::s18::vector<>, sdsl::s18::rank_support<> , sdsl::s18::select_support<>>, uint64_t ,rl_runs< 64,wt_rlmn<sd_vector<>,sd_vector<>::rank_1_type,sd_vector<>::select_1_type,wt_int<>  > > > >  TWA(path_bin,bwt,civ,0);  
 
   char path_mem[100];   
