85055716fab0c34880dedf6909dc74356112621d
Francisco <francisco.riverosc@sansano.usm.cl>
2022-07-28 02:36:47 -0400

    pef

--- src/main.cpp
+++ src/main.cpp
@@ -137,7 +137,8 @@ int main(int argc, char *argv[]) {
   /* ARRLMNS */
   // wt INT
    //char *nasap1 = "ARRLFM_SD_INT";
-   //char *nasap1 = "ARRLFM_PEF_INT";
+  // char *nasap1 = "ARRLFM_PEF_INT";
+   char *nasap1 = "ARRLFM_PEFv2_INT";
  // char *nasap1 = "ARRLFM_S18_INT";
 
   // wt AP
@@ -162,7 +163,7 @@ int main(int argc, char *argv[]) {
 
   // rl RUNS rlmn
    // char *nasap1 = "ARRLFM_SD_RUNS_RLMN";
-   char *nasap1 = "ARRLFM_PEF_RUNS_RLMN";
+  // char *nasap1 = "ARRLFM_PEF_RUNS_RLMN";
   //char *nasap1 = "ARRLFM_S18_RUNS_RLMN";
 
 
@@ -182,7 +183,7 @@ int main(int argc, char *argv[]) {
  
   // wt INT
   // Arrlfm< Asap< SDBV, uint64_t ,wt_int<>> >  TWA(path_bin,bwt,civ,0);  
-   //Arrlfm< Asap< pef_vector_opt< rank_support_v5<1>, select_support_mcl<1>, 1024 >, uint64_t ,wt_int<>> >  TWA(path_bin,bwt,civ,0);  
+  Arrlfm< Asap< pef_vector_opt< rank_support_v5<1>, select_support_mcl<1>, 1024 >, uint64_t ,wt_int<>> >  TWA(path_bin,bwt,civ,0);  
  //  Arrlfm< Asap< BVTtipo2<sdsl::s18::vector<>, sdsl::s18::rank_support<> , sdsl::s18::select_support<>> , uint64_t ,wt_int<>> >  TWA(path_bin,bwt,civ,0);  
   
   // wt AP
@@ -208,7 +209,7 @@ int main(int argc, char *argv[]) {
 
   // rl RUNS rlmn ap
   //Arrlfm< Asap< SDBV, uint64_t ,rl_runs< 64,wt_rlmn<sd_vector<>,sd_vector<>::rank_1_type,sd_vector<>::select_1_type,wt_int<>  > > > >  TWA(path_bin,bwt,civ,0);  
-   Arrlfm< Asap< pef_vector_opt< rank_support_v5<1>, select_support_mcl<1>, 1024 >, uint64_t ,rl_runs< 64,wt_rlmn<sd_vector<>,sd_vector<>::rank_1_type,sd_vector<>::select_1_type,wt_int<>  > > > >  TWA(path_bin,bwt,civ,0);  
+  // Arrlfm< Asap< pef_vector_opt< rank_support_v5<1>, select_support_mcl<1>, 1024 >, uint64_t ,rl_runs< 64,wt_rlmn<sd_vector<>,sd_vector<>::rank_1_type,sd_vector<>::select_1_type,wt_int<>  > > > >  TWA(path_bin,bwt,civ,0);  
    //Arrlfm< Asap< BVTtipo2<sdsl::s18::vector<>, sdsl::s18::rank_support<> , sdsl::s18::select_support<>>, uint64_t ,rl_runs< 64,wt_rlmn<sd_vector<>,sd_vector<>::rank_1_type,sd_vector<>::select_1_type,wt_int<>  > > > >  TWA(path_bin,bwt,civ,0);  
 
   char path_mem[100];   
@@ -222,7 +223,7 @@ int main(int argc, char *argv[]) {
  // myfile2 << TWA.size();
   myfile2 << TWA.size2();
  //cout << TWA.size2();
-if(true){
+if(false){
 
 // PATRONES 
   for(int lp = 4 ; lp < 17 ; lp=lp*2){
