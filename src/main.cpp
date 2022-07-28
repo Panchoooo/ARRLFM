2f5a36617976dfb15796ce9923c0ddf8fa57464a
Francisco <francisco.riverosc@sansano.usm.cl>
2022-07-27 22:43:21 -0400

    pef

--- src/main.cpp
+++ src/main.cpp
@@ -136,7 +136,7 @@ int main(int argc, char *argv[]) {
 
   /* ARRLMNS */
   // wt INT
-   char *nasap1 = "ARRLFM_SD_INT";
+   //char *nasap1 = "ARRLFM_SD_INT";
   // char *nasap1 = "ARRLFM_PEF_INT";
  // char *nasap1 = "ARRLFM_S18_INT";
 
@@ -147,7 +147,7 @@ int main(int argc, char *argv[]) {
 
   // wt RLMN INT
   // char *nasap1 = "ARRLFM_SD_RLMN";
-  // char *nasap1 = "ARRLFM_PEF_RLMN";
+   char *nasap1 = "ARRLFM_PEF_RLMN";
   // char *nasap1 = "ARRLFM_S18_RLMN";
 
   // wt RLMN AP
@@ -181,7 +181,7 @@ int main(int argc, char *argv[]) {
   //Baseline< rl_runs< 64,wt_ap<> >  >  TWA(path_bin,bwt,civ); 
  
   // wt INT
-   Arrlfm< Asap< SDBV, uint64_t ,wt_int<>> >  TWA(path_bin,bwt,civ,0);  
+  // Arrlfm< Asap< SDBV, uint64_t ,wt_int<>> >  TWA(path_bin,bwt,civ,0);  
   // Arrlfm< Asap< pef_vector_opt< rank_support_v5<1>, select_support_mcl<1>, 1024 >, uint64_t ,wt_int<>> >  TWA(path_bin,bwt,civ,0);  
  //  Arrlfm< Asap< BVTtipo2<sdsl::s18::vector<>, sdsl::s18::rank_support<> , sdsl::s18::select_support<>> , uint64_t ,wt_int<>> >  TWA(path_bin,bwt,civ,0);  
   
@@ -192,7 +192,7 @@ int main(int argc, char *argv[]) {
   
   // wt RLMN INT
  //  Arrlfm< Asap< SDBV, uint64_t ,wt_rlmn<sd_vector<>,sd_vector<>::rank_1_type,sd_vector<>::select_1_type,wt_int<>   >  > > TWA(path_bin,bwt,civ,0);  
-  // Arrlfm< Asap<pef_vector_opt< rank_support_v5<1>, select_support_mcl<1>, 1024 >, uint64_t ,wt_rlmn<sd_vector<>,sd_vector<>::rank_1_type,sd_vector<>::select_1_type,wt_int<>   >  > > TWA(path_bin,bwt,civ,0);  
+   Arrlfm< Asap<pef_vector_opt< rank_support_v5<1>, select_support_mcl<1>, 1024 >, uint64_t ,wt_rlmn<sd_vector<>,sd_vector<>::rank_1_type,sd_vector<>::select_1_type,wt_int<>   >  > > TWA(path_bin,bwt,civ,0);  
   // Arrlfm< Asap< BVTtipo2<sdsl::s18::vector<>, sdsl::s18::rank_support<> , sdsl::s18::select_support<>>, uint64_t ,wt_rlmn<sd_vector<>,sd_vector<>::rank_1_type,sd_vector<>::select_1_type,wt_int<>   >  > > TWA(path_bin,bwt,civ,0);  
   
   // wt RLMN AP
