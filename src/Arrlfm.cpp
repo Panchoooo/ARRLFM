c53ee1c854b1fd44b18f7da19737e13058fcfc7a
Francisco <francisco.riverosc@sansano.usm.cl>
2022-10-18 02:31:53 -0300

    a

--- src/Arrlfm.cpp
+++ src/Arrlfm.cpp
@@ -438,7 +438,7 @@ template<class Estructura>
 void Arrlfm<Estructura>::getBVs () { 
 
     for ( auto &x: AWT->bit_vectors ){
-        for (size_t i=0; i < x.second.size(); i+=1)
+        for (size_t i=0; i < x.second->size(); i+=1)
             cout << x.second[i] << " "; 
         }
 }
