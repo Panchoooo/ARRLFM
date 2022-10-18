9a63c6df59ac37f570ba949e990f4221f3a0c4cf
Francisco <francisco.riverosc@sansano.usm.cl>
2022-10-18 01:26:10 -0300

    a

--- Lectura/Lectura.cpp
+++ Lectura/Lectura.cpp
@@ -105,119 +105,119 @@ void load ( string input_file, unsigned method ) {
     // Creamos Texto de simbolos
     ifstream stream(data_folder+input_file);
 
-    fs::create_directories(prepath+input_file);
-    ofstream myfile2 (prepath+input_file+"/Simbolos.txt");
-    ofstream myfileInfo (prepath+input_file+"/InfoTexto.txt");
-    vector<string> words = readWords(stream); // Leemos todas las palabras y las ingresmaos a un arreglo
-    for (const string& e : words) { 
-        if (W2S.find(e) == W2S.end() ) { // Si no existe en el diccionario, lo agregamos y seteamos simbolo nuevo
-            W2S[e] = ultimo_simbolo;
-            ultimo_simbolo+=1;
-        }
-        t.resize(t.size()+1);
-        t[t.size()-1] = W2S[e];
-        cout << W2S[e] << " ";
-        myfile2 << W2S[e] << " ";
-        contador+=1;
-    }
-    contador+=1;
-    myfile2 << 0;
-
-    t0 = t;
-
-    myfileInfo << "Cantidad_simbolos=" << (contador-1) << "\n";
-    myfileInfo << "Simbolos_distintos=" << (ultimo_simbolo-1) << "\n";
-
-    string file = "T0";
-    store_to_file(t0, prepath+input_file+"/"+file);
-
-    t.resize(t.size()+1);
-    t[t.size()-1] = 0;
-
-    ofstream myfile3 (prepath+input_file+"/dict.txt");
-    for (auto const &pair: W2S) {
-        myfile3 << "{" << pair.first << ": " << pair.second << "}\n";
-        std::cout << "{" << pair.first << ": " << pair.second << "}\n";
-    }
-
-    long unsigned int simbolo_anterior = -1 ;
-    int simbolos_unicos = 0;
-    util::bit_compress(t);
-
-    qsufsort::construct_sa(sa, t); // Se crea arreglo de sufijos sa en base a t
-    util::bit_compress(sa);
-    BWT = int_vector<>(sa.size()); 
-    uint64_t i = 0;
-    for ( i = 0; i < sa.size(); i++) { // Generamos BWT y arreglo C en base a sa
-        if(sa[i]!=0){
-            BWT[i] = t[sa[i]-1];
-        }else{
-            BWT[i] = 0;
-        }
-        if(t[sa[i]] != simbolo_anterior){
-            simbolo_anterior = t[sa[i]];
-            simbolos_unicos+=1;
-            C.resize(simbolos_unicos);
-            C[simbolos_unicos-1] = i;
-        }
-    }
-
-    C.resize(C.size()+1);
-    C[C.size()-1] = t.size();
-    
-    //BWT[i]= 0;
-
-    file = "BWT";
-    store_to_file(BWT, prepath+input_file+"/"+file);
-    //int_vector<> bwwt = int_vector<>();
-    //load_from_file(bwwt, "BWT");
-    //cout << "BWT:  " << bwwt.size() << "\n";
-    //for (long unsigned int i = 0; i < bwwt.size(); i++) {
-    //    cout << bwwt[i] << " ";
+    //fs::create_directories(prepath+input_file);
+    //ofstream myfile2 (prepath+input_file+"/Simbolos.txt");
+    //ofstream myfileInfo (prepath+input_file+"/InfoTexto.txt");
+    //vector<string> words = readWords(stream); // Leemos todas las palabras y las ingresmaos a un arreglo
+    //for (const string& e : words) { 
+    //    if (W2S.find(e) == W2S.end() ) { // Si no existe en el diccionario, lo agregamos y seteamos simbolo nuevo
+    //        W2S[e] = ultimo_simbolo;
+    //        ultimo_simbolo+=1;
+    //    }
+    //    t.resize(t.size()+1);
+    //    t[t.size()-1] = W2S[e];
+    //    cout << W2S[e] << " ";
+    //    myfile2 << W2S[e] << " ";
+    //    contador+=1;
     //}
-
-      ofstream myfile4 (prepath+input_file+"/bwtsimbolos.txt");
-    for (long unsigned int i = 0; i < BWT.size(); i++) {
-        myfile4 << BWT[i] << " ";
-    }
-
-   
-    file = "C";
-    store_to_file(C, prepath+input_file+"/"+file);
-    //int_vector<> cc = int_vector<>();
-    //load_from_file(cc, "C");
-    //cout << "C:  " << cc.size() << "\n";
-    //for (long unsigned int i = 0; i < cc.size(); i++) {
-    //    cout << cc[i] << " ";
+    //contador+=1;
+    //myfile2 << 0;
+//
+    //t0 = t;
+//
+    //myfileInfo << "Cantidad_simbolos=" << (contador-1) << "\n";
+    //myfileInfo << "Simbolos_distintos=" << (ultimo_simbolo-1) << "\n";
+//
+    //string file = "T0";
+    //store_to_file(t0, prepath+input_file+"/"+file);
+//
+    //t.resize(t.size()+1);
+    //t[t.size()-1] = 0;
+//
+    //ofstream myfile3 (prepath+input_file+"/dict.txt");
+    //for (auto const &pair: W2S) {
+    //    myfile3 << "{" << pair.first << ": " << pair.second << "}\n";
+    //    std::cout << "{" << pair.first << ": " << pair.second << "}\n";
     //}
-    uint64_t* buffer = new uint64_t[BWT.size()];
-    uint64_t largo_runs_promedio = 0; 
-    uint64_t largo_runs = 0; 
-    uint64_t camtidad_runs = 0; 
-    for (uint64_t i = 0; i < BWT.size(); i++) { 
-
-        if(i>0 && BWT[i] != BWT[i-1]){
-            if(largo_runs > 1 ){
-                //cout << i << " "<< BWT[i-1]<< "  " << BWT[i] <<  "  " << largo_runs << "\n";
-                camtidad_runs += 1;
-                largo_runs_promedio+= largo_runs;
-            }
-            largo_runs = 1 ;
-        }else{
-            largo_runs +=1;
-        }
-        buffer[i] = BWT[i];
-    }
-    FILE * pFile;
-    myfileInfo << "BWT_cantidad_runs=" << (camtidad_runs) << "\n";
-    myfileInfo << "BWT_largo_promedio=" << (largo_runs_promedio/camtidad_runs) << "\n";
-
-    string s = prepath+input_file+"/binario.bin";
-    int n = s.length();
-    char char_array[n + 1];
-    strcpy(char_array, s.c_str());
-    pFile = fopen (char_array, "wb");
-    fwrite (buffer, sizeof(uint64_t), BWT.size(), pFile);
+//
+    //long unsigned int simbolo_anterior = -1 ;
+    //int simbolos_unicos = 0;
+    //util::bit_compress(t);
+//
+    //qsufsort::construct_sa(sa, t); // Se crea arreglo de sufijos sa en base a t
+    //util::bit_compress(sa);
+    //BWT = int_vector<>(sa.size()); 
+    //uint64_t i = 0;
+    //for ( i = 0; i < sa.size(); i++) { // Generamos BWT y arreglo C en base a sa
+    //    if(sa[i]!=0){
+    //        BWT[i] = t[sa[i]-1];
+    //    }else{
+    //        BWT[i] = 0;
+    //    }
+    //    if(t[sa[i]] != simbolo_anterior){
+    //        simbolo_anterior = t[sa[i]];
+    //        simbolos_unicos+=1;
+    //        C.resize(simbolos_unicos);
+    //        C[simbolos_unicos-1] = i;
+    //    }
+    //}
+//
+    //C.resize(C.size()+1);
+    //C[C.size()-1] = t.size();
+    //
+    ////BWT[i]= 0;
+//
+    //file = "BWT";
+    //store_to_file(BWT, prepath+input_file+"/"+file);
+    ////int_vector<> bwwt = int_vector<>();
+    ////load_from_file(bwwt, "BWT");
+    ////cout << "BWT:  " << bwwt.size() << "\n";
+    ////for (long unsigned int i = 0; i < bwwt.size(); i++) {
+    ////    cout << bwwt[i] << " ";
+    ////}
+//
+    //  ofstream myfile4 (prepath+input_file+"/bwtsimbolos.txt");
+    //for (long unsigned int i = 0; i < BWT.size(); i++) {
+    //    myfile4 << BWT[i] << " ";
+    //}
+//
+   //
+    //file = "C";
+    //store_to_file(C, prepath+input_file+"/"+file);
+    ////int_vector<> cc = int_vector<>();
+    ////load_from_file(cc, "C");
+    ////cout << "C:  " << cc.size() << "\n";
+    ////for (long unsigned int i = 0; i < cc.size(); i++) {
+    ////    cout << cc[i] << " ";
+    ////}
+    //uint64_t* buffer = new uint64_t[BWT.size()];
+    //uint64_t largo_runs_promedio = 0; 
+    //uint64_t largo_runs = 0; 
+    //uint64_t camtidad_runs = 0; 
+    //for (uint64_t i = 0; i < BWT.size(); i++) { 
+//
+    //    if(i>0 && BWT[i] != BWT[i-1]){
+    //        if(largo_runs > 1 ){
+    //            //cout << i << " "<< BWT[i-1]<< "  " << BWT[i] <<  "  " << largo_runs << "\n";
+    //            camtidad_runs += 1;
+    //            largo_runs_promedio+= largo_runs;
+    //        }
+    //        largo_runs = 1 ;
+    //    }else{
+    //        largo_runs +=1;
+    //    }
+    //    buffer[i] = BWT[i];
+    //}
+    //FILE * pFile;
+    //myfileInfo << "BWT_cantidad_runs=" << (camtidad_runs) << "\n";
+    //myfileInfo << "BWT_largo_promedio=" << (largo_runs_promedio/camtidad_runs) << "\n";
+//
+    //string s = prepath+input_file+"/binario.bin";
+    //int n = s.length();
+    //char char_array[n + 1];
+    //strcpy(char_array, s.c_str());
+    //pFile = fopen (char_array, "wb");
+    //fwrite (buffer, sizeof(uint64_t), BWT.size(), pFile);
 
 }
 
