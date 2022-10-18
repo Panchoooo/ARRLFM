add235d1ec900af9bedacc81be4743fffe2b1685
Francisco <francisco.riverosc@sansano.usm.cl>
2022-10-18 01:30:07 -0300

    a

--- Lectura/Lectura.cpp
+++ Lectura/Lectura.cpp
@@ -106,7 +106,7 @@ void load ( string input_file, unsigned method ) {
 
 
     // Creamos Texto de simbolos
-    ifstream stream(data_folder+input_file);
+    ifstream stream(data_folder+"/"+input_file);
 
     fs::create_directories(prepath+input_file);
     ofstream myfile2 (prepath+input_file+"/Simbolos.txt");
