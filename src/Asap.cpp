051035f23ac6b7c13ffc19de310c6ccf921d2779
Francisco <francisco.riverosc@sansano.usm.cl>
2022-10-18 19:23:13 -0300

    a

--- src/Asap.cpp
+++ src/Asap.cpp
@@ -48,27 +48,28 @@ Asap<BitVectorClass, IntType, WaveletClass>::Asap ( string input_file, unsigned 
     if ( ! m->is_singleton(x.first) )
       construct_im(s_wt_trees[x.first], x.second, 0);
 
-  int contador = 0;
-  ofstream myfile3 ("datossd.txt");
+  uint64_t contador = 0;
+  ofstream myfile ("BV.dat",ios::binary);
   for ( auto &y: temp_bit_vectors ){
       contador+=1;
   }
-  myfile3 << contador << "\n";
+  //myfile << contador << "\n";
+  myfile.write( (char*)&contador, sizeof(uint64_t)); 
   int breaka = 0;
   for ( auto &x: temp_bit_vectors ){
-      if(breaka == 5){
-          break;
-      }
       //cout << "size: " << x.second.size() << "\n";
       //cout << x.second << "\n"; 
-      for (size_t i=0; i < x.second.size() ; i+=1){
+      uint64_t universo = x.second.size();
+      myfile.write( (char*)&universo , sizeof(uint64_t)); 
+      for (uint64_t i=0; i < x.second.size() ; i+=1){
           if(x.second[i] == 1){
-              myfile3 << i << " ";
+              //myfile << i << " ";
+              myfile.write( (char*)&i , sizeof(uint64_t)); 
           }
       }
-      myfile3 << "\n";
-      breaka += 1;
+      //myfile << "\n";
   }
+  myfile.close();
 
   for ( auto &x: temp_bit_vectors ){
     //cout << "x : " << x.first << "\n";
