f8c3a97390ec3df421d89dea19297e7df9820429
Francisco <francisco.riverosc@sansano.usm.cl>
2022-11-19 18:13:51 -0300

    a

--- src/Asap.cpp
+++ src/Asap.cpp
@@ -42,9 +42,9 @@ Asap<BitVectorClass, IntType, WaveletClass>::Asap ( string input_file, unsigned 
     l = m->map(s[i]);
     temp_bit_vectors[l][i] = 1; // <---
     s_vectors[l][s_indexes[l]++] = m->rank(m->get_pos_by_char(s[i]), l);
-    /*if(i+1 == text_length){
+    if(i+1 == text_length){
       s_vectors[l][s_indexes[l]++] = m->rank(m->get_pos_by_char(s[i]), l);
-    }*/
+    }
   }
 
   for ( auto &x: s_vectors )
