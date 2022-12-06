ed50e233a4e2f69e00a899c5835bed296cdafbf7
Francisco <francisco.riverosc@sansano.usm.cl>
2022-12-05 21:41:55 -0300

    a

--- makefile
+++ makefile
@@ -65,7 +65,7 @@ run:
 #	./$(EXEC) coreutils 202
 
 #	./$(EXEC) coreutils 100
-	./$(EXEC) coreutils 101
+#	./$(EXEC) coreutils 101
 #	./$(EXEC) coreutils 102
 #	./$(EXEC) coreutils 300
 #	./$(EXEC) coreutils 301
@@ -174,6 +174,7 @@ run:
 #	./$(EXEC) einstein.de.txt 100
 #	./$(EXEC) einstein.de.txt 101
 #	./$(EXEC) einstein.de.txt 102
+#	./$(EXEC) einstein.de.txt 103
 #	./$(EXEC) einstein.de.txt 300
 #	./$(EXEC) einstein.de.txt 301
 #	./$(EXEC) einstein.de.txt 302
@@ -211,6 +212,7 @@ run:
 #	./$(EXEC) einstein.en.txt 100
 #	./$(EXEC) einstein.en.txt 101
 #	./$(EXEC) einstein.en.txt 102
+	./$(EXEC) einstein.en.txt 103
 #	./$(EXEC) einstein.en.txt 300
 #	./$(EXEC) einstein.en.txt 301
 #	./$(EXEC) einstein.en.txt 302
--- src/main.cpp
+++ src/main.cpp
@@ -468,6 +468,31 @@ int main(int argc, char *argv[]) {
     ofstream myfile2 (path_mem);
     myfile2 << TWA.size();
   }
+  if(tipo == 103){
+    char *nasap1 = "ARRLFM_S18_RLMNs";
+    strcpy(path_asap1,path_output);
+    strcat(path_asap1,nasap1); 
+    strcat(path_asap1,separate2); 
+    strcat(path_asap1,nasap1); 
+    strcpy(path_asapfolder,path_output);
+    strcat(path_asapfolder,nasap1); 
+    cout << path_bin << "\n";
+    fs::create_directories(path_asapfolder); // Crear carpeta si no existe
+    Arrlfm< Asap< BVTtipo2<sdsl::s18::vector<8>, sdsl::s18::rank_support<1,8> , sdsl::s18::select_support<1,8>>, uint64_t ,wt_rlmn<>  > > TWA(path_bin,bwt,civ,2);  
+    // Tiempos
+    TWA.testing(file,nasap1,lectura_path);
+    // Memoria 
+    char path_mem[100];   
+    strcpy(path_mem,lectura_path);
+    strcat(path_mem,file); 
+    strcat(path_mem,"/Memoria/"); 
+    strcat(path_mem,nasap1); 
+    strcat(path_mem,"_Memoria.txt"); 
+    cout << path_mem << "\n";
+    ofstream myfile2 (path_mem);
+    myfile2 << TWA.size();
+  }
+
 
   // RLE || RLMN
   if(tipo == 300){
